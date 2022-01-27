73681a3e86ec766056ff3b73422d83d35754f2c4
c0r37py <83533187+c0r37py@users.noreply.github.com>
2022-01-27 07:02:33 +0500

    Create Landingattributes.h

---
+++ Landingattributes.h
@@ -0,0 +1,15 @@
+//This is the landing Page to drive you to the gamemode base class.
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LitePC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LitePC_API APUBGLGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
